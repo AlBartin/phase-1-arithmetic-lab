2e7d6d3dddcac6ad3fe323bab2009f37fc1350e7
alan <alan.martnz@gmail.com>
2022-04-25 22:54:44 -0700

    commit tree 8701d87220adecd5a9b6f02099f9bc843ae1f342

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
 
 // Write your code here
+let num1 = 31;
+let num2 = 2;
+let num3 = 19;
+let num4 = 5;
+
+
+
+let multiply = num1 * num2;
+
+let random = Number.parseInt(Math.random() +1, 10);
+
+let mod = num3 % num4;
+
+const max = Math.max(2, 13, 4, 20);
